cb897078546596ff076922510edf5364881d7257
Victor Nagahama <victorh.nagahama@gmail.com>
2022-03-28 19:30:12 +0100

    fix gp_pred

--- codes/likelihood_comparison.R
+++ codes/likelihood_comparison.R
@@ -2,7 +2,6 @@
 #
 # The basic idea is that the NNGP likelihood should be a cheaper approximation
 # of the GP likelihood.
-# 
 # In order to assess, we write the joint distribution of NNGP as the product of 
 # conditionals, that is
 #
@@ -11,36 +10,45 @@
 # where N(y_k) is the neighbor set of y_k.
 
 library(dplyr)
+library(ggplot2)
 library(mvtnorm)
 source("./nngp-notes/codes/nn_matrix.R")
 
-n <- 5
+n <- 1000
 m <- 3
 
 set.seed(126)
 coords <- cbind(runif(n), runif(n))
 
+# To do: make generic to data without ordering
 ord <- order(coords[,1])
 coords <- coords[ord,]
 
 sigma_sq <- 2
-phi <- 3
+phi_sq <- 3
 tau_sq <- 0.9
 
 d <- dist(coords) %>%
   as.matrix()
 
-w <- rmvnorm(1, rep(0, n), sigma_sq * exp(-phi * d)) %>%
+w <- rmvnorm(1, rep(0, n), sigma_sq * exp(-phi_sq * d)) %>%
   c()
 eps <- rnorm(n, sqrt(tau_sq))
 y <- w + eps
 
-# Predicting from a GP
-gp_pred <- function(y_new, y, sigma, coords){
-  d2 <- rbind(y_new, coords) %>%
+# Conditional distribution p(y_star | y) from a MVN
+gp_pred <- function(y_new, y, p, coords){
+  d <- dist(coords) %>%
+    as.matrix()
+  d_star <- rbind(y_new, coords) %>%
     dist() %>%
     as.matrix()
-  sigma_star <- sigma_sq * exp(-phi * d2[-1, 1])
+  d_star <- d_star[-1, 1]
+  sigma_sq <- p[1]
+  phi_sq <- p[2]
+  tau_sq <- p[3]
+  sigma <- sigma_sq * exp(-phi_sq * d) + tau_sq * diag(length(y))
+  sigma_star <- sigma_sq * exp(-phi_sq * d_star)
   sigma_star_star <- sigma_sq
   ynew_mean <- sigma_star %*% solve(sigma, y)
   ynew_sigma <- sigma_star_star - sigma_star %*% solve(sigma, sigma_star)
@@ -48,31 +56,17 @@ gp_pred <- function(y_new, y, sigma, coords){
   return(y_param)
 }
 
-sigma <- sigma_sq * exp(-phi * d) + tau_sq * diag(n)
-
-# ------------------------------------------
-# Full GP
-# -----------------------------------------
-sapply(2:n, function(i) gp_pred(coords[i,], y[1:i], sigma[1:i, 1:i], coords[1:i,])) %>%
-  t()
-
-# ------------------------------------------
-# NNGP
-# -----------------------------------------
-nn_data <- get_nn(coords, m)
-for (i in 1:nrow(nn_data$nn_idx)) {
-  nn <- nn_data$ord[nn_data$nn_idx[i,]]
-  print(gp_pred(coords[i + 1,], y[nn], sigma[nn, nn], coords[nn,]))
-}
-
 # GP likelihood
 gp_ll = function(p, x, y) {
-  sigma_sq = exp(p[1])
-  phi = exp(p[2])
-  tau_sq = exp(p[3])
+  sigma_sq <- p[1] %>%
+    as.numeric()
+  phi_sq <- p[2] %>%
+    as.numeric()
+  tau_sq <- p[3] %>%
+    as.numeric()
   d <- dist(x) %>%
     as.matrix()
-  sigma <- sigma_sq * exp(-phi * d) + tau_sq * diag(nrow(x))
+  sigma <- sigma_sq * exp(-phi_sq * d) + tau_sq * diag(nrow(x))
   mu <- rep(0, nrow(x))
   ll <- dmvnorm(y, mu, sigma, log = TRUE)
   return(-ll)
@@ -80,18 +74,20 @@ gp_ll = function(p, x, y) {
 
 # NNGP likelihood
 nngp_ll = function(p, x, y, m) {
-  sigma_sq = exp(p[1])
-  phi = exp(p[2])
-  tau_sq = exp(p[3])
-  d <- dist(x) %>%
-    as.matrix()
-  sigma <- sigma_sq * exp(-phi * d) + tau_sq * diag(nrow(x))
-  nn_data <- get_nn(coords, m)
+  sigma_sq <- p[1] %>%
+    as.numeric()
+  phi_sq <- p[2] %>%
+    as.numeric()
+  tau_sq <- p[3] %>%
+    as.numeric()
+  nn_data <- get_nn(x, m)
   cond_dens <- NULL
-  cond_dens[1] <- dnorm(y[1], 0, sqrt(sigma[1, 1]))
+  cond_dens[1] <- dnorm(y[1], 0, sqrt(sigma_sq + tau_sq))
+  # Get the conditional distributions for NNGP
   for (i in 1:nrow(nn_data$nn_idx)) {
     nn <- nn_data$ord[nn_data$nn_idx[i,]]
-    parms <- gp_pred(coords[i + 1,], y[nn], sigma[nn, nn], coords[nn,])
+    parms <- gp_pred(
+      x[i + 1,], y[nn], c(sigma_sq, phi_sq, tau_sq), matrix(x[nn,], nrow = length(nn)))
     cond_dens[i + 1] <- dnorm(y[i + 1], parms[1], sqrt(parms[2]))
   }
   ll <- prod(cond_dens) %>%
@@ -99,6 +95,20 @@ nngp_ll = function(p, x, y, m) {
   return(-ll)
 }
 
-parms <- log(c(2, 3, 0.9))
-gp_ll(parms, coords, y)
-nngp_ll(parms, coords, y, m)
+parms_grid <- seq(0.2, sigma_sq + 2, length.out = 20) %>%
+  tibble(sigma_sq = ., phi_sq, tau_sq)
+
+gp_uni <- apply(parms_grid, MARGIN = 1, FUN = gp_ll, coords, y)
+nngp_uni <- apply(parms_grid, MARGIN = 1, FUN = nngp_ll, coords, y, m)
+
+parms_grid <- parms_grid %>%
+  bind_cols("gp" = gp_uni, "nngp" = nngp_uni)
+
+ggplot(parms_grid, aes(x = sigma_sq, y = gp, colour = "GP")) +
+  geom_line() +
+  geom_line(aes(y = nngp, colour = "NNGP")) +
+  geom_vline(xintercept = parms_grid$sigma_sq[which.min(parms_grid$gp)], colour = "red") +
+  geom_vline(xintercept = parms_grid$sigma_sq[which.min(parms_grid$nngp)], colour = "blue") +
+  labs(x = expression(sigma^2), y = "log-likelihood") +
+  theme_light()
+
