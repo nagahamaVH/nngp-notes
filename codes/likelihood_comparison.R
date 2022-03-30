fb39f9cb59046f112dd70341c7895110258822ae
Victor Nagahama <victorh.nagahama@gmail.com>
2022-03-31 00:24:04 +0100

    parametrization, code revision

--- codes/likelihood_comparison.R
+++ codes/likelihood_comparison.R
@@ -14,7 +14,7 @@ library(ggplot2)
 library(mvtnorm)
 source("./nngp-notes/codes/nn_matrix.R")
 
-n <- 1000
+n <- 5
 m <- 3
 
 set.seed(126)
@@ -24,16 +24,16 @@ coords <- cbind(runif(n), runif(n))
 ord <- order(coords[,1])
 coords <- coords[ord,]
 
-sigma_sq <- 2
-phi_sq <- 3
-tau_sq <- 0.9
+sigma <- 3
+phi <- 4
+tau <- 0.6
 
 d <- dist(coords) %>%
   as.matrix()
 
-w <- rmvnorm(1, rep(0, n), sigma_sq * exp(-phi_sq * d)) %>%
+w <- rmvnorm(1, sigma = sigma^2 * exp(-phi^2 * d)) %>%
   c()
-eps <- rnorm(n, sqrt(tau_sq))
+eps <- rnorm(n, 0, tau)
 y <- w + eps
 
 # Conditional distribution p(y_star | y) from a MVN
@@ -44,50 +44,49 @@ gp_pred <- function(y_new, y, p, coords){
     dist() %>%
     as.matrix()
   d_star <- d_star[-1, 1]
-  sigma_sq <- p[1]
-  phi_sq <- p[2]
-  tau_sq <- p[3]
-  sigma <- sigma_sq * exp(-phi_sq * d) + tau_sq * diag(length(y))
-  sigma_star <- sigma_sq * exp(-phi_sq * d_star)
-  sigma_star_star <- sigma_sq
-  ynew_mean <- sigma_star %*% solve(sigma, y)
-  ynew_sigma <- sigma_star_star - sigma_star %*% solve(sigma, sigma_star)
+  sigma <- p[1]
+  phi <- p[2]
+  tau <- p[3]
+  Sigma <- sigma^2 * exp(-phi^2 * d) + tau^2 * diag(length(y))
+  sigma_star <- sigma^2 * exp(-phi^2 * d_star)
+  sigma_star_star <- sigma^2
+  ynew_mean <- sigma_star %*% solve(Sigma, y)
+  ynew_sigma <- sigma_star_star - sigma_star %*% solve(Sigma, sigma_star)
   y_param <- c(ynew_mean, ynew_sigma)
   return(y_param)
 }
 
 # GP likelihood
 gp_ll = function(p, x, y) {
-  sigma_sq <- p[1] %>%
+  sigma <- p[1] %>%
     as.numeric()
-  phi_sq <- p[2] %>%
+  phi <- p[2] %>%
     as.numeric()
-  tau_sq <- p[3] %>%
+  tau <- p[3] %>%
     as.numeric()
   d <- dist(x) %>%
     as.matrix()
-  sigma <- sigma_sq * exp(-phi_sq * d) + tau_sq * diag(nrow(x))
+  Sigma <- sigma^2 * exp(-phi^2 * d) + tau^2 * diag(nrow(x))
   mu <- rep(0, nrow(x))
-  ll <- dmvnorm(y, mu, sigma, log = TRUE)
+  ll <- dmvnorm(y, mu, Sigma, log = TRUE)
   return(-ll)
 }
 
 # NNGP likelihood
 nngp_ll = function(p, x, y, m) {
-  sigma_sq <- p[1] %>%
+  sigma <- p[1] %>%
     as.numeric()
-  phi_sq <- p[2] %>%
+  phi <- p[2] %>%
     as.numeric()
-  tau_sq <- p[3] %>%
+  tau <- p[3] %>%
     as.numeric()
   nn_data <- get_nn(x, m)
-  cond_dens <- NULL
-  cond_dens[1] <- dnorm(y[1], 0, sqrt(sigma_sq + tau_sq))
+  cond_dens <- dnorm(y[1], 0, sqrt(sigma^2 + tau^2))
   # Get the conditional distributions for NNGP
   for (i in 1:nrow(nn_data$nn_idx)) {
     nn <- nn_data$ord[nn_data$nn_idx[i,]]
     parms <- gp_pred(
-      x[i + 1,], y[nn], c(sigma_sq, phi_sq, tau_sq), matrix(x[nn,], nrow = length(nn)))
+      x[i + 1,], y[nn], c(sigma, phi, tau), matrix(x[nn,], nrow = length(nn)))
     cond_dens[i + 1] <- dnorm(y[i + 1], parms[1], sqrt(parms[2]))
   }
   ll <- prod(cond_dens) %>%
@@ -95,8 +94,8 @@ nngp_ll = function(p, x, y, m) {
   return(-ll)
 }
 
-parms_grid <- seq(0.2, sigma_sq + 2, length.out = 20) %>%
-  tibble(sigma_sq = ., phi_sq, tau_sq)
+parms_grid <- seq(0.2, sigma + 2, length.out = 20) %>%
+  tibble(sigma = ., phi, tau)
 
 gp_uni <- apply(parms_grid, MARGIN = 1, FUN = gp_ll, coords, y)
 nngp_uni <- apply(parms_grid, MARGIN = 1, FUN = nngp_ll, coords, y, m)
@@ -104,11 +103,13 @@ nngp_uni <- apply(parms_grid, MARGIN = 1, FUN = nngp_ll, coords, y, m)
 parms_grid <- parms_grid %>%
   bind_cols("gp" = gp_uni, "nngp" = nngp_uni)
 
-ggplot(parms_grid, aes(x = sigma_sq, y = gp, colour = "GP")) +
+ggplot(parms_grid, aes(x = sigma, y = gp, colour = "GP")) +
   geom_line() +
   geom_line(aes(y = nngp, colour = "NNGP")) +
-  geom_vline(xintercept = parms_grid$sigma_sq[which.min(parms_grid$gp)], colour = "red") +
-  geom_vline(xintercept = parms_grid$sigma_sq[which.min(parms_grid$nngp)], colour = "blue") +
-  labs(x = expression(sigma^2), y = "log-likelihood") +
+  geom_point(data = parms_grid[which.min(parms_grid$gp),]) +
+  geom_point(
+    data = parms_grid[which.min(parms_grid$nngp),],
+    aes(y = nngp, colour = "NNGP")) +
+  labs(x = expression(sigma), y = "log-likelihood", colour = "") +
   theme_light()
 
