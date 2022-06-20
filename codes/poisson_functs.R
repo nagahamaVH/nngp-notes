7553209b5a7963b7df42f90c5af14ca43555e982
Victor Nagahama <victorh.nagahama@gmail.com>
2022-06-20 21:02:10 +0100

    poisson sanity check

--- codes/poisson_functs.R
+++ codes/poisson_functs.R
@@ -9,8 +9,8 @@ library(msm)
 # -----------------------------------------------------------------------------
 
 # f(w | y, theta) \propto f(y | w, theta) * f(w | theta)
-posterior <- function(y, w, mu = rep(0, length(w)), sigma, log = F) {
-  ll <- sum(dpois(y, exp(w), log = T)) + dmvnorm(w, mu, sigma, log = T)
+posterior <- function(y, w, sigma_w, mu = rep(0, length(w)), log = F) {
+  ll <- sum(dpois(y, exp(w), log = T)) + dmvnorm(w, mu, sigma_w, log = T)
   if (!log) {
     ll <- exp(ll)
   }
@@ -31,100 +31,75 @@ proxy_fyw0 <- function(y, w0){
   c <- -hessian(w0)
   c <- ifelse(c < 0, sqrt(.Machine$double.xmin), c)
   mu <- b / c
-  
+
   parms <- list(b = b, c = c, mu = mu)
   return(parms)
 }
 
-# # Checking for n = 2
-# w0 <- rep(0, n)
-# for (i in 1:100) {
-#   proxy <- proxy_fyw0(y, w0)
-#   e <- abs(w0 - proxy$mu) %>%
-#     sum() / length(w0)
-#   if (e < 1e-4) {
-#     print("Converged!")
-#     break
-#   }
-#   w0 <- proxy$mu
-# }
-# 
-# # Exact
-# w_grid <- sapply(w, function(w) seq(w - w/2, w + w/2, length.out = 50)) %>%
-#   as_tibble() %>%
-#   expand.grid()
-# dens <- apply(w_grid, MARGIN = 1, function(x){
-#   dpois(y, exp(x), log = T) %>%
-#     sum()
-# })
-# df <- bind_cols(
-#   w_grid,
-#   d = dens
-# )
-# 
-# df <- bind_cols(
-#   w_grid,
-#   exact = apply(w_grid, MARGIN = 1, function(x){
-#     dpois(y, as.numeric(exp(x)), log = T) %>%
-#       sum()
-#   }),
-#   proxy = apply(w_grid, MARGIN = 1, dmvnorm, mean = proxy$mu, sigma = diag(1 / proxy$c), log = T)
-# )
-# 
-# # Visualising the approximation
-# library(plotly)
-# plot_ly() %>%
-#   add_trace(data = df, x = ~V1, y = ~V2, z = ~exact, type = "mesh3d") %>%
-#   add_trace(data = df, x = ~V1, y = ~V2, z = ~proxy, type = "mesh3d")
-
 # Gaussian proxy of f(w | y) using Taylor expansion
-proxy_fwy0 <- function(mu_w, sigma_w, b_y, c_y){
-  prec_m <- solve(sigma_w)
-  b <- prec_m %*% mu_w + b_y
-  c <- prec_m + diag(c_y, length(mu_w))
+# proxy_fwy0 <- function(mu_w, sigma_w, b_y, c_y){
+#   prec_m <- solve(sigma_w)
+#   b <- prec_m %*% mu_w + b_y
+#   c <- prec_m + diag(c_y, length(mu_w))
+# 
+#   mu <- solve(c, b) %>%
+#     as.vector()
+#   parms <- list(mu = mu, sigma = solve(c))
+#   return(parms)
+# }
 
-  mu <- solve(c, b) %>%
-    as.vector()
-  parms <- list(mu = mu, sigma = solve(c))
-  return(parms)
-}
+# MODIFY TO ANY MU_W
+# proxy_fwy02 <- function(sigma_w, y, w0){
+#   hess_m <- diag(hessian(w0), length(w0))
+#   sigma <- solve(solve(sigma_w) - hess_m)
+#   mu <- sigma %*% (gradient(w0, y) - hess_m %*% w0)
+#   parms <- list(mu = as.vector(mu), sigma = sigma)
+#   return(parms)
+# }
 
 estim_gaussian_proxy <- function(
     w0, y, sigma_w, mu_w = rep(0, length(y)), control = list(it = 100, tol = 1e-4)) {
-  
-  # Taylor expansion of f(y | w) to approximate to a normal distribution
-  fit_fyw0 <- proxy_fyw0(y, w0)
-  
-  # Calculating the parameters. Note that f(w | y) is already Gaussian
-  fit_fwy0 <- proxy_fwy0(mu_w, sigma_w, fit_fyw0$b, fit_fyw0$c)
-  
+  for (i in 1:control$it) {
+    # Taylor expansion of f(y | w) to approximate to a normal distribution
+    # fit_fyw0 <- proxy_fyw0(y, w0)
+
+    # Calculating the parameters. Note that f(w | y) is already Gaussian
+    # fit_fwy0 <- proxy_fwy0(mu_w, sigma_w, fit_fyw0$b, fit_fyw0$c)
+
+    hess_m <- diag(hessian(w0), length(w0))
+    sigma <- solve(solve(sigma_w) - hess_m)
+    mu <- as.vector(sigma %*% (gradient(w0, y) - hess_m %*% w0))
+
+    # if (max(abs(w0 - fit_fwy0$mu)) < control$tol) break
+    if (max(abs(w0 - mu)) < control$tol) break
+    if (i == control$it) stop("Max iteraction number reached")
+    # w0 <- fit_fwy0$mu
+    w0 <- mu
+  }
+
+  # parms <- list(
+    # mu = fit_fwy0$mu, sigma = fit_fwy0$sigma)
   parms <- list(
-    mu = fit_fwy0$mu, sigma = fit_fwy0$sigma)
-  
+    mu = mu, sigma = sigma)
   return(parms)
 }
 
 # Laplace approximation
 # f(theta | y) \approx f(y | w_hat, theta) * f(w_hat | theta) / MVN(w_hat, sigma)
-laplace <- function(y, coords, p_init, w_hat) {
+laplace <- function(y, coords, p_init, w_hat, sigma_hat, mu_w = rep(0, length(y))) {
   sigma <- exp(p_init[1])
   phi <- exp(p_init[2])
-  mu <- rep(0, length(y))
   d <- dist(coords) %>%
     as.matrix()
   sigma_w <- sigma^2 * exp(-phi^2 * d)
   
-  # Transform singular matrix to non-singular. What about SVD?
-  # while (det(sigma_w) == 0) {
-  #   sigma_w <- sigma_w + diag(1e-3, nrow(d))
-  # }
-    
   # Covariance matrix of Gaussian proxy
-  prec_proxy <- solve(sigma_w) + diag(-hessian(w_hat), length(w_hat))
-  sigma_proxy <- solve(prec_proxy)
+  hess_m <- diag(hessian(w_hat), length(w_hat))
+  sigma_proxy <- solve(solve(sigma_w) - hess_m)
 
   denom <- -0.5 * log(det(sigma_proxy) + sqrt(.Machine$double.xmin))
-  ll <- posterior(y, w_hat, mu, sigma_w, log = T) - denom
+  
+  ll <- posterior(y, w_hat, sigma_w, mu_w, log = T) - denom
 
   return(ll)
 }
--- codes/poisson_sim.R
+++ codes/poisson_sim.R
@@ -21,35 +21,36 @@ m <- 3
 
 set.seed(126)
 coords <- cbind(runif(n), runif(n))
+# coords <- cbind(rep(1:10, each = 10), rep(1:10, 10))
 
 # To do: make generic to data without ordering
 ord <- order(coords[,1])
 coords <- coords[ord,]
 
 # Parameters
-sigma_true <- 1.7
-phi_true <- 3.2
-
+sigma_true <- 1
+phi_true <- 2.3
+mu_w <- 0
 # Generate data
 d <- dist(coords) %>%
   as.matrix()
-w <- rmvnorm(1, sigma = sigma_true^2 * exp(-phi_true^2 * d)) %>%
+w <- rmvnorm(1, mean = rep(mu_w, n), sigma = sigma_true^2 * exp(-phi_true^2 * d)) %>%
   c()
 # C <- sigma^2 * exp(-phi^2 * d)
 # w <- c(t(chol(C)) %*% rnorm(n))
 y <- rpois(n, exp(w))
 
+hist(y)
+
 table(y == 0) %>%
   prop.table()
-
 # -----------------------
 # Comparing estimate of spatial effect
 # -----------------------
-
 # GP
 n_it <- 100
 p_init <- c(log(1), log(1))
-w_init <- log(y + sqrt(.Machine$double.xmin))
+w_init <- log(y + 1e-4)
 for (i in 1:n_it) {
   print(i)
   sigma <- exp(p_init[1])
@@ -57,13 +58,15 @@ for (i in 1:n_it) {
   sigma_w <- sigma^2 * exp(-phi^2 * d)
   
   # Gaussian proxy
-  gauss <- estim_gaussian_proxy(w_init, y, sigma_w)
+  gauss <- estim_gaussian_proxy(w_init, y, mu_w = rep(mu_w, n), sigma_w)
   w_hat <- gauss$mu
+  sigma_hat <- gauss$sigma
   
   # Laplace
   fitted_parms <- optim(
-    p_init, laplace, y = y, coords = coords, w_hat = w_hat,
-    method = "BFGS", hessian = T, control = list(fnscale = -1))
+    p_init, laplace, y = y, coords = coords, w_hat = w_hat, 
+    sigma_hat = sigma_hat, mu_w = rep(mu_w, n), method = "BFGS", hessian = T, 
+    control = list(fnscale = -1))
   if (fitted_parms$convergence != 0) stop("Convergence error Laplace")
   
   if (max(abs(p_init - fitted_parms$par)) < 1e-4) break
@@ -73,10 +76,8 @@ for (i in 1:n_it) {
   print(exp(fitted_parms$par))
 }
 
-fitted_parms
-
 sigma_hat <- deltamethod(
-  list(~log(x1), ~log(x2)), fitted_parms$par, -solve(fitted_parms$hessian), ses = F)
+  list(~exp(x1), ~exp(x2)), fitted_parms$par, -solve(fitted_parms$hessian), ses = F)
 
 ci_parms <- confint2(exp(fitted_parms$par), sigma_hat) %>%
   mutate(
@@ -89,4 +90,5 @@ ggplot(data = ci_parms, aes(x = "")) +
   geom_linerange(aes(ymin = lb, ymax = ub)) +
   geom_point(aes(y = mu)) +
   geom_hline(aes(yintercept = true), lty = 2, col = "red") +
-  labs(y = "Estimate")
+  labs(y = "Estimate", x = "")
+
